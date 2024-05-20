344898f84a01c7c4d807c392dfdf037f722e9342
mikfairo <mikfairo123@gmail.com>
2024-05-20 15:54:04 -0400

    Update servoproject2024.ino

--- servoproject2024.ino
+++ servoproject2024.ino
@@ -1,67 +1,100 @@
 #include <Servo.h>
-Servo myservo; 
-const int buttonPin = 4; 
+
+Servo myservo;
+const int buttonPin = 4;
 const int buttonPin2 = 2;
-const int LED =  13; 
+const int LED = 13;
 const int LED2 = 12;
-const int LED3 = 11;    
+const int LED3 = 11;
 const int LED4 = 10;
 const int LED5 = 9;
 
-int LEDState = LOW;       
-int buttonState = LOW;  
-int buttonState2 = LOW;           
-int lastButtonState = LOW;   
-int pos = 0;
+int LEDState = LOW;
+int buttonState = LOW;
+int buttonState2 = LOW;
+int lastButtonState = LOW;
 
-// setup runs once at startup
 void setup() {
-  pinMode(buttonPin, INPUT); 
-  pinMode(buttonPin2,INPUT);
-  pinMode(LED, OUTPUT);  
-  pinMode (LED2, OUTPUT);
-  pinMode (LED3, OUTPUT);
-  pinMode (LED4, OUTPUT);
-  pinMode (LED5,OUTPUT);
+  pinMode(buttonPin, INPUT);
+  pinMode(buttonPin2, INPUT);
+  pinMode(LED, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
+  pinMode(LED5, OUTPUT);
   myservo.attach(8);
+  randomSeed(analogRead(0)); // Initialize random seed
 }
 
-void move_servo(int pos)
-{
+void move_servo(int pos) {
   myservo.write(pos);
   delay(1000);
 }
+
+void execute_sequence(int sequence) {
+  switch (sequence) {
+    case 1:
+      move_servo(0);
+      move_servo(90);
+      move_servo(180);
+      move_servo(90);
+      break;
+    case 2:
+      move_servo(0);
+      move_servo(45);
+      move_servo(90);
+      move_servo(180);
+      break;
+    case 3:
+      move_servo(0);
+      move_servo(90);
+      move_servo(180);
+      move_servo(45);
+      break;
+    case 4:
+      move_servo(0);
+      move_servo(45);
+      move_servo(45);
+      move_servo(90);
+      break;
+    case 5:
+      move_servo(0);
+      move_servo(180);
+      move_servo(90);
+      move_servo(45);
+      break;
+  }
+}
+
 void loop() {
-
-  for (pos = 0; pos <= 180; pos += 90) { // goes from 0 degrees to 180 degrees
-    // in steps of 90 degree         
-    move_servo(0);
-    move_servo(90);
-    move_servo(180);
-    move_servo(90);            // waits 1s for the servo to reach the position
-  }
-  buttonState = digitalRead(buttonPin); 
+  buttonState = digitalRead(buttonPin);
   buttonState2 = digitalRead(buttonPin2);
-  delay (4000);
 
-  if (buttonState == HIGH && lastButtonState == LOW) {
-    LEDState = !LEDState; 
-  } else {
+  // Move servo with a random sequence when buttonPin is pressed
+  if (buttonState == HIGH) {
+    int sequence = random(1, 6); // Randomly select a sequence from 1 to 5
+    execute_sequence(sequence);
   }
-  lastButtonState = buttonState; 
-  if (LEDState == 1) { 
-    digitalWrite (LED, HIGH); 
-    digitalWrite (LED2, HIGH);
-    digitalWrite (LED3, HIGH);
-    digitalWrite (LED4, HIGH);
-    digitalWrite (LED5, HIGH);  
 
+  // Toggle LED state when buttonPin2 is pressed
+  if (buttonState2 == HIGH && lastButtonState == LOW) {
+    LEDState = !LEDState;
+  }
+  lastButtonState = buttonState2;
+
+  if (LEDState == HIGH) {
+    digitalWrite(LED, HIGH);
+    digitalWrite(LED2, HIGH);
+    digitalWrite(LED3, HIGH);
+    digitalWrite(LED4, HIGH);
+    digitalWrite(LED5, HIGH);
   } else {
-    digitalWrite (LED, LOW); 
-    digitalWrite (LED2,LOW);
-    digitalWrite (LED3,LOW);
+    digitalWrite(LED, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
     digitalWrite(LED4, LOW);
     digitalWrite(LED5, LOW);
-
   }
+
+  delay(50); // Add a small delay to debounce the buttons
 }
